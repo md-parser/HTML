e0caf85b5f464bc011c7f41db2b5b8979e7d2fef
Marten de Groot <mdegroot@xebia.com>
2023-04-20 15:27:29 +0200

    Updated test

--- src/tests/commonMark/commonMark.test.ts
+++ src/tests/commonMark/commonMark.test.ts
@@ -1,24 +1,14 @@
-import { parseMarkdown } from '../../parseMarkdown';
+import { markdownParser } from '../../render';
 import cases from './cases.v0.30.json';
 
 describe('commonMark tests', () => {
   console.log({ cases });
 
   cases.map((testcase) => {
-    it(`${testcase.section} | ${testcase.markdown}`, () => {
-      const ast = parseMarkdown(testcase.markdown);
+    it(`Example ${testcase.example} | ${testcase.section} | ${testcase.markdown}`, () => {
+      const html = markdownParser({ children: testcase.markdown, components: {} });
 
-      expect(ast).toEqual([
-        {
-          type: 'paragraph',
-          children: [
-            {
-              type: 'text',
-              value: 'Hello world',
-            },
-          ],
-        },
-      ]);
+      expect(testcase.html).toEqual(html);
     });
   });
 });
